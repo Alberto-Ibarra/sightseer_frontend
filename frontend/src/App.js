f5f0b82d1954cb368c99c24d847957d794c8c4fb
ialoysious <isaacaloysious@gmail.com>
2023-03-13 10:12:57 -0700

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-
-import React, {useState, useEffect} from 'react'
-
-import axios from 'axios'
-import CreateTrip from './components/CreateTrip';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+// need database state
+// need new trip state
+// wrap createtrip jsx in a state to display
 function App() {
+	const [trips,setTrips] = useState([]);
+	const [newtrip, setNewTrip] = useState({})
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>SightSeer</h1>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
 
-  const [trips, setTrips] = useState([])
-  const [newTrip, setNewTrip] = useState({
-    country:'', 
-    continent: '',
-    rating: 0,
-    season: '',
-    image: '',
-    description: ''
-  })
-
-  // const handleTripChange = (e) => {
-  //   setNewTrip({...newTrip, [e.target.name]: e.target.value})
-  // }
-
+export default App;
 	const handleTripChange = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
@@ -27,13 +35,3 @@ function App() {
 			[name]: value,
 		}));
 	};
-
-  return (
-    <div>
-      <h1>Test</h1>
-      <CreateTrip handleTripChange={handleTripChange}/>
-    </div>
-  );
-}
-
-export default App;
