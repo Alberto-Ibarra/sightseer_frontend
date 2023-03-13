f71d65b434a1a6bb6451d2215b95237e05f31935
ialoysious <isaacaloysious@gmail.com>
2023-03-13 14:19:47 -0700

    create trip changes, ffunctions in component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,34 +12,53 @@ function App() {
 		image: '',
 		description: '',
 	});
+	const [updated,setUpdated] = useState(false);
 
 	// const handleTripChange = (e) => {
 	//   setNewTrip({...newTrip, [e.target.name]: e.target.value})
 	// }
-
-	const handleTripChange = (e) => {
-		const name = e.target.name;
-		const value = e.target.value;
-		setNewTrip((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-	const handleNewTripSubmit = (e) => {
-		e.preventDefault();
-		axios.post('http://localhost:3000/sights', newTrip).then(() => {
-			axios.get('http://localhost:3000/sights').then((res) => {
-				setTrips(res.data);
-			});
+	const getTrips = () => {
+		axios.get('http://localhost:3000/sights').then((res)=> {
+			setTrips(res.data)
 		});
 	};
+	useEffect(()=> {
+		getTrips();
+	},[])
+	useEffect(()=> {
+		getTrips();
+	},[updated])
+
+	// const handleTripChange = (e) => {
+	// 	const name = e.target.name;
+	// 	const value = e.target.value;
+	// 	setNewTrip((prevState) => ({
+	// 		...prevState,
+	// 		[name]: value,
+	// 	}));
+	// };
+	// const handleNewTripSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	axios.post('http://localhost:3000/sights', newTrip).then(() => {
+	// 		axios.get('http://localhost:3000/sights').then((res) => {
+	// 			setTrips(res.data);
+	// 		});
+	// 	});
+	// };
 
 	return (
 		<div>
 			<h1>Test</h1>
 			<CreateTrip
-				handleTripChange={handleTripChange}
-				handleNewTripSubmit={handleNewTripSubmit}
+				// handleTripChange={handleTripChange}
+				// handleNewTripSubmit={handleNewTripSubmit}
+				// trips={trips}
+				trips={trips}
+				newTrip={newTrip}
+				setNewTrip={setNewTrip}
+				setTrips={setTrips}
+				setUpdated={setUpdated}
+				updated={updated}
 			/>
 		</div>
 	);
--- frontend/src/components/CreateTrip.jsx
+++ frontend/src/components/CreateTrip.jsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const CreateTrip = (props) => {
+	const handleTripChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		props.setNewTrip((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
+	const handleNewTripSubmit = (e) => {
+		e.preventDefault();
+		axios.post('http://localhost:3000/sights', props.newTrip).then(() => {
+			axios.get('http://localhost:3000/sights').then((res) => {
+				props.setTrips(res.data);
+				props.setUpdated(!props.updated);
+        props.setNewTrip({
+					country: '',
+					continent: '',
+					rating: 0,
+					season: '',
+					image: '',
+					description: '',
+				});
+			});
+		});
+	};
 	return (
 		<>
 			<div>Create Trip</div>
-			<form onSubmit={props.handleNewTripSubmit}>
+			<form onSubmit={handleNewTripSubmit}>
 				<label>Country</label>
-				<input type='text' name='country' onChange={props.handleTripChange} />
+				<input type='text' name='country' onChange={handleTripChange} />
 
 				<label>Continent</label>
-				<input type='text' name='continent' onChange={props.handleTripChange} />
+				<input type='text' name='continent' onChange={handleTripChange} />
 
 				<label>Season</label>
-				<input type='text' name='season' onChange={props.handleTripChange} />
+				<input type='text' name='season' onChange={handleTripChange} />
 
 				<label>Image</label>
-				<input type='text' name='image' onChange={props.handleTripChange} />
+				<input type='text' name='image' onChange={handleTripChange} />
 
 				<label>Rating</label>
-				<input type='number' name='rating' onChange={props.handleTripChange} />
+				<input type='number' name='rating' onChange={handleTripChange} />
 
 				<label>Description</label>
-				<textarea name='description' onChange={props.handleTripChange} />
+				<textarea name='description' onChange={handleTripChange} />
 				<input type='submit' value='Add Trip' />
 			</form>
 		</>
