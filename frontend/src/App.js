0b0726066b2d9f5a6295e36238ba77c9ef824029
ialoysious <isaacaloysious@gmail.com>
2023-03-13 16:53:17 -0700

    fixed my devil code and never want to see it again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import "./App.css"
 import axios from 'axios';
 import CreateTrip from './components/CreateTrip';
-import DisplayData from './components/DisplayData'
+import DisplayData from './components/DisplayData';
 
 function App() {
 	const [trips, setTrips] = useState([]);
@@ -14,24 +14,50 @@ function App() {
 		image: '',
 		description: '',
 	});
-	const [updated,setUpdated] = useState(false);
+	const [updated, setUpdated] = useState(false);
 
 	// const handleTripChange = (e) => {
 	//   setNewTrip({...newTrip, [e.target.name]: e.target.value})
 	// }
 	const getTrips = () => {
-		axios.get('http://localhost:3000/sights').then((res)=> {
-			setTrips(res.data)
+		axios.get('http://localhost:3000/sights').then((res) => {
+			setTrips(res.data);
 		});
 	};
 
+	const handleUpdate = (e, trip) => {
+		e.preventDefault();
+		axios
+			.put(`http://localhost:3000/sights/${trip._id}`, { ...trip })
+			.then(() => {
+				axios.get('http://localhost:3000/sights').then((res) => {
+					setTrips(res.data);
+				});
+			});
+	};
 
-	useEffect(()=> {
+	const handleInputEdit = (e, id) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setTrips((prevState) =>
+			prevState.map((trip) => {
+				if (trip._id === id) {
+					return {
+						...trip,
+						[name]: value,
+					};
+				} else {
+					return trip;
+				}
+			})
+		);
+	};
+	useEffect(() => {
 		getTrips();
-	},[])
-	useEffect(()=> {
+	}, []);
+	useEffect(() => {
 		getTrips();
-	},[updated])
+	}, [updated]);
 
 	// const handleTripChange = (e) => {
 	// 	const name = e.target.name;
@@ -63,18 +89,18 @@ function App() {
 				setUpdated={setUpdated}
 				updated={updated}
 			/>
-      <section>
-        {
-          trips.map((trip) => {
-            return(
-             <DisplayData 
-             setTrips={setTrips}
-             trip = {trip}
-             />
-            )
-          })
-        }
-      </section>
+			<section>
+				{trips.map((trip) => {
+					return (
+						<DisplayData
+							setTrips={setTrips}
+							trip={trip}
+							handleInputEdit={handleInputEdit}
+							handleUpdate={handleUpdate}
+						/>
+					);
+				})}
+			</section>
 		</div>
 	);
 }
--- frontend/src/components/DisplayData.jsx
+++ frontend/src/components/DisplayData.jsx
@@ -1,54 +1,65 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 
 const DisplayData = (props) => {
-    const [editedTrip, setEditedTrip] = useState({});
+	const handleDelete = (siteData) => {
+		axios.delete(`http://localhost:3000/sights/${siteData._id}`).then(() => {
+			axios.get(`http://localhost:3000/sights`).then((res) => {
+				props.setTrips(res.data);
+			});
+		});
+	};
 
-    const handleDelete = (siteData) => {
-        axios.delete(`http://localhost:3000/sights/${siteData._id}`)
-        .then(()=> {
-            axios.get(`http://localhost:3000/sights`)
-            .then((res) => {
-            props.setTrips(res.data)
-            })
-        })
-    };
-
-    const handleUpdate = (e, trip) => {
-        e.preventDefault();
-        axios.put(`http://localhost:3000/sights/${trip._id}`, editedTrip)
-            .then(() => {
-                axios.get(`http://localhost:3000/sights`)
-                .then((res) => {
-                    props.setTrips(res.data)
-                })
-            })
-    }
-
-    const handleInputEdit = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setEditedTrip((prevState)=>({
-            ...prevState,
-            [name]: value,
-        }))
-    }
-    
-    console.log(props.trip._id)
-
-    return(
-        <div>
-            <p>{props.trip.country}</p>
-            <p>{props.trip.continent}</p>
-            <button onClick={(e) => handleDelete(props.trip)}>DELETE</button>
-
-            <form onSubmit={(e)=> handleUpdate(e, props.trip)}>
-                <input type='text' name='country' placeholder={props.trip.continent} onChange={handleInputEdit}/>
-                <input type='text' name='continent' placeholder={props.trip.country} onChange={handleInputEdit}/>
-                <button type='submit'>Update</button>
-            </form>
-        </div>
-    )
-}
+	return (
+		<div>
+			<form onSubmit={(e) => props.handleUpdate(e, props.trip)}>
+				<p>{props.trip.country}</p>
+				<p>{props.trip.continent}</p>
+				<button onClick={(e) => handleDelete(props.trip)}>DELETE</button>
+				<input
+					type='text'
+					placeholder={props.trip.continent}
+					onChange={(e) => props.handleInputEdit(e, props.trip._id)}></input>
+				<input
+					type='text'
+					name='country'
+					value={props.trip.country}
+					onChange={(e) => props.handleInputEdit(e, props.trip._id)}
+				/>
+				<button onClick={() => props.handleUpdate(props.trip._id)}>
+					Update
+				</button>
+			</form>
+		</div>
+	);
+};
 
 export default DisplayData;
+
+// const handleUpdate = (e, trip) => {
+// 	e.preventDefault();
+// 	axios
+// 		.put(`http://localhost:3000/sights/${trip._id}`, trip )
+// 		.then(() => {
+// 			axios.get(`http://localhost:3000/sights`).then((res) => {
+// 				props.setTrips(res.data);
+// 			});
+// 		});
+// };
+
+// const handleInputEdit = (e, id) => {
+// 	const name = e.target.name;
+// 	const value = e.target.value;
+// 	props.setTrips((prevState) =>
+// 		prevState.map((trip) => {
+// 			if (trip._id === id) {
+// 				return {
+// 					...trip,
+// 					[name]: value,
+// 				};
+// 			} else {
+// 				return trip;
+// 			}
+// 		})
+// 	);
+// };
