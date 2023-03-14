998ef7bfab85cdff616a3a40cc5a3dfe542e3fd6
ryanmoon19 <ryanhmoon11@gmail.com>
2023-03-14 10:47:48 -0700

    commit before pull

--- frontend/src/components/CreateTrip.jsx
+++ frontend/src/components/CreateTrip.jsx
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 import axios from 'axios';
 // import '../App.css'
 
-const CreateTrip = (props) => {
+const CreateTrip = ({ newTrip, setNewTrip, setTrips, updated, setUpdated }) => {
 	const handleTripChange = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
-		props.setNewTrip((prevState) => ({
+		setNewTrip((prevState) => ({
 			...prevState,
 			[name]: value,
 		}));
 	};
 	const handleNewTripSubmit = (e) => {
 		e.preventDefault();
-		axios.post('http://localhost:3000/sights', props.newTrip).then(() => {
+		axios.post('http://localhost:3000/sights', newTrip).then(() => {
 			axios.get('http://localhost:3000/sights').then((res) => {
-				props.setTrips(res.data);
-				props.setUpdated(!props.updated);
-        props.setNewTrip({
+				setTrips(res.data);
+				setUpdated(!updated);
+        setNewTrip({
 					country: '',
 					continent: '',
 					rating: 0,
@@ -30,28 +30,47 @@ const CreateTrip = (props) => {
 	};
 	return (
 		<>
-		<div className='container'>
-			<div className="title">Publish Your Trip</div>
-			<form className="card" onSubmit={handleNewTripSubmit}>
-				<label>Country</label>
-				<input type='text' name='country' onChange={handleTripChange} />
+			<div className='container'>
+				<div className='title'>Publish Your Trip</div>
+				<form className='card' onSubmit={handleNewTripSubmit}>
+					<label>Country</label>
+					<input
+						type='text'
+						name='country'
+						value={newTrip.country}
+						onChange={handleTripChange}
+					/>
 
-				<label>Continent</label>
-				<input type='text' name='continent' onChange={handleTripChange} />
+					<label>Continent</label>
+					<input
+						type='text'
+						name='continent'
+						value={newTrip.continent}
+						onChange={handleTripChange}
+					/>
 
-				<label>Season</label>
-				<input type='text' name='season' onChange={handleTripChange} />
+					<label>Season</label>
+					<input
+						type='text'
+						name='season'
+						value={newTrip.season}
+						onChange={handleTripChange}
+					/>
 
-				<label>Image</label>
-				<input type='text' name='image' onChange={handleTripChange} />
+					<label>Image</label>
+					<input type='text' name='image' onChange={handleTripChange} />
 
-				<label>Rating</label>
-				<input type='number' name='rating' onChange={handleTripChange} />
+					<label>Rating</label>
+					<input type='number' name='rating' onChange={handleTripChange} />
 
-				<label>Description</label>
-				<textarea name='description' onChange={handleTripChange} />
-				<input className="button" type='submit' value='Add Trip' />
-			</form>
+					<label>Description</label>
+					<textarea
+						name='description'
+						value={newTrip.image}
+						onChange={handleTripChange}
+					/>
+					<input className='button' type='submit' value='Add Trip' />
+				</form>
 			</div>
 		</>
 	);
--- frontend/src/components/DisplayData.jsx
+++ frontend/src/components/DisplayData.jsx
@@ -1,57 +1,65 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const DisplayData = (props) => {
-    const [editedTrip, setEditedTrip] = useState({});
+	const [editedTrip, setEditedTrip] = useState({});
 
-    const handleDelete = (siteData) => {
-        axios.delete(`http://localhost:3000/sights/${siteData._id}`)
-        .then(()=> {
-            axios.get(`http://localhost:3000/sights`)
-            .then((res) => {
-            props.setTrips(res.data)
-            })
-        })
-    };
+	const handleDelete = (siteData) => {
+		axios.delete(`http://localhost:3000/sights/${siteData._id}`).then(() => {
+			axios.get(`http://localhost:3000/sights`).then((res) => {
+				props.setTrips(res.data);
+			});
+		});
+	};
 
-    const handleUpdate = (e, trip) => {
-        e.preventDefault();
-        axios.put(`http://localhost:3000/sights/${trip._id}`, editedTrip)
-            .then(() => {
-                axios.get(`http://localhost:3000/sights`)
-                .then((res) => {
-                    props.setTrips(res.data)
-                })
-            })
-    }
+	const handleUpdate = (e, trip) => {
+		e.preventDefault();
+		axios
+			.put(`http://localhost:3000/sights/${trip._id}`, editedTrip)
+			.then(() => {
+				axios.get(`http://localhost:3000/sights`).then((res) => {
+					props.setTrips(res.data);
+				});
+			});
+	};
 
-    const handleInputEdit = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setEditedTrip((prevState)=>({
-            ...prevState,
-            [name]: value,
-        }))
-    }
-    
-    // console.log(props.trip._id)
+	const handleInputEdit = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setEditedTrip((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
 
-    return(
-        <div>
-            <div>
-                <img src={props.trip.image} className={props.className} />
-            </div>
-            {/* <p>{props.trip.country}</p>
-            <p>{props.trip.continent}</p>
-            <button onClick={(e) => handleDelete(props.trip)}>DELETE</button>
+	// console.log(props.trip._id)
 
-            <form onSubmit={(e)=> handleUpdate(e, props.trip)}>
-                <input type='text' name='country' placeholder={props.trip.continent} onChange={handleInputEdit}/>
-                <input type='text' name='continent' placeholder={props.trip.country} onChange={handleInputEdit}/>
-                <button type='submit'>Update</button>
-            </form> */}
-        </div>
-    )
-}
+	return (
+		<div>
+			<div>
+				<img src={props.trip.image} className={props.className} />
+			</div>
+			<p>{props.trip.country}</p>
+			<p>{props.trip.continent}</p>
+			<button onClick={(e) => handleDelete(props.trip)}>DELETE</button>
+
+			<form onSubmit={(e) => handleUpdate(e, props.trip)}>
+				<input
+					type='text'
+					name='country'
+					placeholder={props.trip.continent}
+					onChange={handleInputEdit}
+				/>
+				<input
+					type='text'
+					name='continent'
+					placeholder={props.trip.country}
+					onChange={handleInputEdit}
+				/>
+				<button type='submit'>Update</button>
+			</form>
+		</div>
+	);
+};
 
 export default DisplayData;
