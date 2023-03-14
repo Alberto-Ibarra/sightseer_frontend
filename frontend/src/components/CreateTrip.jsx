ba023cc8cd11f5932778532fa3d65156475378c0
ialoysious <isaacaloysious@gmail.com>
2023-03-14 09:55:50 -0700

    input fix

--- frontend/src/components/CreateTrip.jsx
+++ frontend/src/components/CreateTrip.jsx
@@ -3,37 +3,43 @@ import axios from "axios";
 // import '../App.css'
 
 const CreateTrip = ({ newTrip, setNewTrip, setTrips, updated, setUpdated }) => {
-  const handleTripChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setNewTrip((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-  const handleNewTripSubmit = (e) => {
-    e.preventDefault();
-    axios.post("http://localhost:3000/sights", newTrip).then(() => {
-      axios.get("http://localhost:3000/sights").then((res) => {
-        setTrips(res.data);
-        setUpdated(!updated);
-        setNewTrip({
-          country: "",
-          continent: "",
-          rating: 0,
-          season: "",
-          image: "",
-          description: "",
-        });
-      });
-    });
-  };
-  return (
-    <>
-      <div className="container">
-        <h2 >Publish Your Trip</h2>
-        <form className="card" onSubmit={handleNewTripSubmit}>
-          <label>Country</label>
+	const handleTripChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setNewTrip((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
+	const handleNewTripSubmit = (e) => {
+		e.preventDefault();
+		axios.post('http://localhost:3000/sights', newTrip).then(() => {
+			axios.get('http://localhost:3000/sights').then((res) => {
+				setTrips(res.data);
+				setUpdated(!updated);
+				setNewTrip({
+					country: '',
+					continent: '',
+					rating: 0,
+					season: '',
+					image: '',
+					description: '',
+				});
+			});
+		});
+	};
+	return (
+		<>
+			<div className='container'>
+				<div className='title'>Publish Your Trip</div>
+				<form className='card' onSubmit={handleNewTripSubmit}>
+					<label>Country</label>
+					<input
+						type='text'
+						name='country'
+						value={newTrip.country}
+						onChange={handleTripChange}
+					/>
 
           <input
             type="text"
@@ -67,11 +73,32 @@ const CreateTrip = ({ newTrip, setNewTrip, setTrips, updated, setUpdated }) => {
           />
 
           <label>Image</label>
-          <input type="text" name="image" onChange={handleTripChange} />
+          <input
+						type="text"
+						name="image"
+						value={newTrip.image}
+						onChange={handleTripChange}
+					/>
 
           <label>Rating</label>
-          <input type="number" name="rating" onChange={handleTripChange} />
+          <input
+						type="number"
+						name="rating"
+						value={newTrip.rating}
+						onChange={handleTripChange}
+					/>
 
+					<label>Description</label>
+					<textarea
+						name='description'
+						value={newTrip.description}
+						onChange={handleTripChange}
+					/>
+					<input className='button' type='submit' value='Add Trip' />
+				</form>
+			</div>
+		</>
+	);
           <label>Description</label>
           <textarea
             name="description"
